6ddfadcb66301576108baaec6a348276307ae7ba
RahulR <rash235r@gmail.com>
2018-12-04 10:03:09 +0400

    summary page styles

--- client/src/components/atoms/QuestionSummary/index.js
+++ client/src/components/atoms/QuestionSummary/index.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-
+import style from './style.scss';
 class QuestionSummary extends Component {
   render() {
     return (
-      <dir>
-        <div>{this.props.question}</div>
-        <div>{this.props.answer}</div>
-      </dir>
+      <div>
+        <div className={style.question}>{this.props.question}</div>
+        <div className={style.answer}>{this.props.answer}</div>
+      </div>
     );
   }
 }
